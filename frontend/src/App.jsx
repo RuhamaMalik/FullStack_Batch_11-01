80f30f67f2eac49864d8df31cf1b70df03a18e21
Kavita-LachmanDas <kavitaluhana11@gmail.com>
2025-07-10 08:58:47 -0700

    Added new feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import TopProducts from './components/TopProducts'
 
 const App = () => {
   return (
-    // <Login/>
-    <Register/>
-    // <div className='bg-red-500'>App</div>
+
+    <>
+     <Register/>
+     <TopProducts/>
+       {/*  <Login/>
+   
+    
+     <div className='bg-red-500'>App</div> */}
+    </>
+  
   )
 }
 
