cb3ce83478fcfa9325e24a09141c8b89b028464d
Nazibi97 <nazbibi103@gmail.com>
2025-07-11 22:24:25 +0500

    footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Footer from './components/Footer'
+import ContactInfo from './components/Footer'
 
 const App = () => {
   return (
     // <Login/>
-    <Register/>
+    // <Register/>
     // <div className='bg-red-500'>App</div>
+    <Footer/>
+   
   )
 }
 
